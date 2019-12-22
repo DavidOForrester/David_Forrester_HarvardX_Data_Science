cff90e0262bb1ac2b4d0cbd14c288b219c69509f
David Forrester <davidforrester@Davids-MBP.modem>
2019-12-22 16:40:51 +0800

    First Upload

---
+++ Project.R
@@ -0,0 +1,26 @@
+library(tidyverse)
+library(lubridate)
+library(stringr)
+library(rvest)
+library(XML)
+library(tidytext)
+library(wordcloud)
+library(dslabs)
+library(caret)
+
+################################
+# Create edx set, validation set
+################################
+
+# Note: this process could take a couple of minutes
+
+if(!require(tidyverse)) install.packages("tidyverse", repos = "http://cran.us.r-project.org")
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(data.table)) install.packages("data.table", repos = "http://cran.us.r-project.org")
+
+# MovieLens 10M dataset:
+# https://grouplens.org/datasets/movielens/10m/
+# http://files.grouplens.org/datasets/movielens/ml-10m.zip
+
+dl <- tempfile()
+download.file("http://files.grouplens.org/datasets/movielens/ml-10m.zip", dl)
