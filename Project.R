234837cbfad06f167607c3e652e3c3bfb0a94fde
David Forrester <davidforrester@Davids-MBP.modem>
2019-12-23 09:45:25 +0800

    split data set and started on first prediction

--- Project.R
+++ Project.R
@@ -17,3 +17,31 @@ file <- ("aapl.us.txt")
 
 # Read csv file into environment
 aapl <- read_csv(file)
+
+# Split the data set by date ranges 80% train 20% test
+aapl_train <- aapl[1:round(nrow(aapl) * 0.8),]
+aapl_test <- na.omit(aapl[round(nrow(aapl) * 0.8) + 1:nrow(aapl),])
+
+# Test for the number of rows in each set 
+nrow(aapl_train)
+nrow(aapl_test)
+
+# Top 6 rows from the training data
+head(aapl_train)
+
+# plot of training data over time
+aapl_train %>%
+  ggplot() +
+  geom_line(aes(Date, Close))
+  
+# Using the previous value as the prediction for the current value
+aapl_train <- mutate(aapl_train, PrevClose = lag(Close))
+
+# plot of training data over time with the first prediction
+aapl_train %>%
+  ggplot() +
+  geom_line(aes(Date, Close, colour = "red")) +
+  geom_line(aes(Date, PrevClose, colour = "blue")) 
+
+
+
